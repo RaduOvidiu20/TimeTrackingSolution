96b4702ea695317e802eca513bd1d5e3123a6046
Radu Ovidiu <ovidiuradu93@gmail.com>
2023-06-12 00:22:42 +0300

    migrations added

--- TimeTracking/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TimeTracking.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
